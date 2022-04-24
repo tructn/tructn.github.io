ff5e6518819812568beed325093edbbc2c47d8e2
Truc Nguyen <truc.nguyentrung@hotmail.com>
2022-04-24 11:51:22 +0100

    fix breakwords

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -13,7 +13,7 @@ export default function Post({postData}) {
             <Head>
                 <title>{postData.title}</title>
             </Head>
-            <article className="break-all">
+            <article className="break-words">
                 <div className="mb-5">
                     <h1 className={utilStyles.headingXl}>{postData.title}</h1>
                     <div className={utilStyles.lightText}>
